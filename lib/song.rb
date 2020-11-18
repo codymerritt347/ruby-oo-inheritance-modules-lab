83e678da77737ba59d2d993500ac6d24a10eb7b8
Cody Merritt <cody.merritt@me.com>
2020-11-17 22:51:07 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,15 @@ require 'pry'
 
 class Song
 
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
   include Paramable
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
